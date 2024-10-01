1fce2b51cbd214c8c446737e46ebf5ec3706e1a5
Liu-Fernando <fernandoliu@usp.br>
2024-09-30 23:36:24 -0300

    Feito interface da tela de estatísticas com gráfico de pizza

---
+++ app/src/main/java/com/example/dopaminho/PieChartData.kt
@@ -0,0 +1,18 @@
+package com.example.dopaminho
+
+// on below line we are creating data class for
+// pie chart data and passing variable as browser
+// name and value.
+data class PieChartData(
+    var browserName: String?,
+    var value: Float?
+)
+
+// on below line we are creating a method
+// in which we are passing all the data.
+val getPieChartData = listOf(
+    PieChartData("TikTok", 34.68F),
+    PieChartData("Instagram", 16.60F),
+    PieChartData("Youtube", 16.15F),
+    PieChartData("Facebook", 15.62F),
+)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,7 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+
     }
 }
 dependencyResolutionManagement {
@@ -16,8 +17,12 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven {
+            url = uri( "https://jitpack.io")
+        }
     }
 }
+
 
 rootProject.name = "dopaminho"
 include(":app")
